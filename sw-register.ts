9b1b4d13ceb8dd693edc8ef2f3bd539217ae4dbe
Treystu <Treystu@users.noreply.github.com>
2025-09-01 21:02:51 -1000

    Register SW as module for dev and prod

--- sw-register.ts
+++ sw-register.ts
@@ -1,8 +1,9 @@
 // Registers the basic SW for offline support.
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
+    const url = import.meta.env.DEV ? '/sw.ts' : '/sw.js';
     navigator.serviceWorker
-      .register('/sw.ts', { type: 'module' })
+      .register(url, { type: 'module' })
       .catch(console.error);
   });
 }
